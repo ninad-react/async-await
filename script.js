9808bc50bb71cbddb3450ed5946adaccf04012a2
ninad-react <157863834+ninad-react@users.noreply.github.com>
2024-03-15 12:07:09 +0530

    Async Await

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
+// // always retruns a promise
+// async function getData() {
+//     // return "Namaste";
+//     return p;
+// }
+
+// const dataPromise = getData();
+// dataPromise.then((resp) => console.log(resp));
+
 const p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("Promise resolved value !!")
-    }, 5000);
-    
+    }, 5000)
 })
 
 const p2 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("Promise resolved value !!")
-    }, 10000);
-    
+    }, 10000)
 })
 
+//handling promises using async await
 
-// always returns a promise
-// async function getData() {
-//     // return "Namaste";  
-//     return p;  
-// }
-
-// await can only be used inside an async function
+// await can only be used inside async function
 async function handlePromise() {
 
-    //JS engine was waiting for promise to be resolved
+    console.log("Hello world");
+
     const val = await p1;
     console.log("Namaste JavaScript");
-    console.log('val', val);
+    console.log(val);
 
     const val2 = await p2;
     console.log("Namaste JavaScript 2");
-    console.log('val2', val2);
+    console.log(val2);
 }
+
 handlePromise();
 
-// const dataPromise = getData();
-// dataPromise.then((res) => console.log(res));
 
-// function getData(){
-
-//     //JS engine will not wait for promise to be resolved
-//     p.then((res) => console.log(res));
-//     console.log("Namaste Javascript");
-// }
+//handling promises using normal function
+function getData() {
+    p.then((res) => console.log('res', res));
+    console.log("Namaste JavaScript");
+}
 
 // getData();
+
